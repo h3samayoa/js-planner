3ad92bd8edfeec5629d9b54fd05e4c55af9f8383
h3samayoa <h3samayoa@gmail.com>
2021-09-12 16:59:38 -0700

    added js to set time-blocks past/present/future and changed ids of time blocks to match moment().hour()

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,46 @@
-var currentDate = moment().format('dddd, MMM, YYYY');
+var currentDate = moment().format('dddd, MMMM Do');
+var saveBtn = document.getElementsByClassName("saveBtn");
+
 document.querySelector("#currentDay").innerText = currentDate;
+
+$(document).ready(function () {
+
+    $(".saveBtn").on('click', function() {
+    
+        var descriptionText = $('.description').val();
+        var descriptionTime = $(this).parent().attr("id")
+    
+        localStorage.setItem(descriptionText, descriptionTime);
+        console.log(descriptionText, descriptionTime);
+    })
+    
+    function timer() {
+        var currentTime = moment().hour();
+    
+        $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id"));
+            
+            if (timeBlock < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            } else if (timeBlock === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+    
+        })
+    }
+
+    timer();
+    
+})
+
+
+
+
